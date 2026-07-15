23ac539562182871d39487e3a9aa4141a2a3cb40
Meet <meet11india@gmail.com>
2026-07-15 11:44:11 +0530

    fix: import useState in index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
     Linking,
     RefreshControl,
